cd47f9319667fcd573edbefd95a0addc0fc744bb
Caroline Wang <cxw320@case.edu>
2023-08-31 17:24:03 -0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+//run app is required always, provided by flutter, all about getting the app up and running
+// you must wrap this in other functions
+// main is function name, void is return type, curly braces is function body
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 91, 19, 216),
+                Color.fromARGB(255, 195, 181, 219),
+              ],
+            ),
+          ),
+          child: const Center(
+            child: Text("helloooooo"),
+          ),
+        ),
+      ),
+    ),
+  );
+}
