2d2b9e602db3490f980ec4e41d70c75347132393
Caroline Wang <cxw320@case.edu>
2023-09-01 03:36:22 -0400

    add dice image

---
+++ lib/gradient_container.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  final Color color1;
+  final Color color2;
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [color1, color2],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: Center(
+        child: Image.asset('assets/images/dice-2.png', width: 200),
+      ),
+    );
+  }
+}
+
+class StyledText extends StatelessWidget {
+  const StyledText(this.text, {super.key});
+
+  final String text;
+
+  @override
+  Widget build(context) {
+    return Text(
+      text,
+      style: const TextStyle(
+        color: Colors.white,
+        fontSize: 28,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,16 @@
 import 'package:flutter/material.dart';
 
+import 'gradient_container.dart';
+
 //run app is required always, provided by flutter, all about getting the app up and running
 // you must wrap this in other functions
 // main is function name, void is return type, curly braces is function body
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 91, 19, 216),
-                Color.fromARGB(255, 195, 181, 219),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text("helloooooo"),
-          ),
-        ),
+        body: GradientContainer(
+            Colors.deepPurple, Color.fromARGB(255, 90, 60, 143)),
       ),
     ),
   );
